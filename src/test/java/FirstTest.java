e518f5fcdc28c14fc47bfe64f4bf6374ab1fd93a
lucian popa <popaluci1998@gmail.com>
2024-10-24 15:36:49 +0300

    added gitignore and updated endava phone number test

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class FirstTest {
